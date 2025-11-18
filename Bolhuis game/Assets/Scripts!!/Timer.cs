fa0dda7c2d072cc3987cf7028c4edbb31d997852
MarijnLubbers <mlubbers5@st.noorderpoort.nl>
2025-11-18 10:07:36 +0100

    timer

---
+++ Bolhuis game/Assets/Scripts!!/Timer.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float remainingTime = 60f;
+
+    private Label timerLabel;
+
+    void Awake()
+    {
+        var uiDoc = GetComponent<UIDocument>();
+
+        timerLabel = uiDoc.rootVisualElement.Q<Label>("timer-label");
+
+        if (timerLabel == null)
+        {
+            Debug.LogError("Label 'timer-label' niet gevonden! Controleer de naam in UI Builder.");
+        }
+    }
+
+    void Update()
+    {
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else if (remainingTime < 0)
+        {
+            remainingTime = 0;
+            timerLabel.style.color = Color.red;
+        }
+
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+
+        if (timerLabel != null)
+            timerLabel.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
